b6bcac89b491c7d46399f4adddf23ab7555109d6
Wesley White <wesleyrwhite86@gmail.com>
2020-03-16 12:20:42 -1000

    Implemented WordsTableViewController

--- Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
+++ Swift Vocabulary/Swift Vocabulary/VocabularyWord.swift
@@ -8,26 +8,7 @@
 
 import UIKit
 
-class VocabularyWord: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        struct VocabularyWord {
-            word: String
-            definition: String
-        }
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+struct VocabularyWord {
+    word: String
+    definition: String
 }
